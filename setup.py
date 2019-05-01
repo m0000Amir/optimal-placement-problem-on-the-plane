3ed2b59ee9445efbb5cf05d563f6630844f31dfc
AmirMukhtarov <40074001+AmirMukhtarov@users.noreply.github.com>
2019-05-01 23:59:31 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='optimal-placement-problem-on-the-plane',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='MukhtarovAA',
+    author_email='mukhtarov.amir.a@gmail.com',
+    description=''
+)
